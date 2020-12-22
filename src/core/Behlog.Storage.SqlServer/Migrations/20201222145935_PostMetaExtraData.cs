6380edc9eb442e8aa518831b066bd1d9c722e55f
iman nemati <imun22@gmail.com>
2020-12-22 18:30:25 +0330

    Add-migration PostMetaExtraData

---
+++ src/core/Behlog.Storage.SqlServer/Migrations/20201222145935_PostMetaExtraData.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Behlog.Storage.SqlServer.Migrations
+{
+    public partial class PostMetaExtraData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverPhoto",
+                table: "Content.Post_Meta",
+                maxLength: 4000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IconName",
+                table: "Content.Post_Meta",
+                maxLength: 300,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNumber",
+                table: "Content.Post_Meta",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Content.Post_Meta",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Status",
+                value: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverPhoto",
+                table: "Content.Post_Meta");
+
+            migrationBuilder.DropColumn(
+                name: "IconName",
+                table: "Content.Post_Meta");
+
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "Content.Post_Meta");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Content.Post_Meta");
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "System.PostTypes",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Status",
+                value: 1);
+        }
+    }
+}
