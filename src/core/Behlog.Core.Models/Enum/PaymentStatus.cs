ec91bccfbd6b1c22a70fda4eeb1490432bb9736e
iman nemati <imun22@gmail.com>
2021-01-03 18:43:58 +0330

    Add PaymentStatus enum

---
+++ src/core/Behlog.Core.Models/Enum/PaymentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Behlog.Core.Models.Enum {
+    public enum PaymentStatus {
+        Deleted = -1,
+        Unsuccessful = 0,
+        Successful = 1,
+        SuccessfulAndFullyPaid = 2,
+        SuccessfulButHasRemaining = 3
+    }
+}
